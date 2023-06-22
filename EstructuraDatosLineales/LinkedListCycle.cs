cb4f616d99f3f2c583f0cfdd6233f81af1773de7
LuisiitoDev <luissandoval1897@hotmail.com>
2023-06-21 21:47:26 -0600

    Add project files.

---
+++ EstructuraDatosLineales/LinkedListCycle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstructuraDatosLineales
+{
+    internal class Node
+    {
+        public int Valor { get; set; }
+        public Node Siguiente { get; set; }
+    }
+
+    internal class LinkedListCycle
+    {
+        public static bool HasCycle(Node nodo)
+        {
+            if (nodo is null) return false;
+
+            var lento = nodo;
+            var rapido = nodo.Siguiente;
+
+            while (lento != rapido)
+            {
+                if (lento is null || rapido.Siguiente is null)
+                    return false;
+
+                lento = lento.Siguiente;
+                rapido = rapido.Siguiente.Siguiente;
+            }
+
+            return true;
+        }
+    }
+}
